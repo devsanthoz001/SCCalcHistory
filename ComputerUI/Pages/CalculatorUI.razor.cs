70ed0895714260cb0d9950d0ae8ed81f72193434
devsanthoz <devsanthoz@DESKTOP-KMRF5J5.localdomain>
2023-09-22 01:06:22 +0530

    added operators in  calculator

--- ComputerUI/Pages/CalculatorUI.razor.cs
+++ ComputerUI/Pages/CalculatorUI.razor.cs
@@ -1,6 +1,9 @@
+using ProjectCalculator;
+
 namespace ComputerUI.Pages;
 public partial class CalculatorUI
 {
+    Calculator calculatorInstance = new Calculator();   
     public string UserHasTyped { get; set; } = string.Empty;
     private string lastPressedKey = string.Empty;
     public void DeleteCalculatorHistory()
@@ -20,9 +23,28 @@ public partial class CalculatorUI
     {
         UserHasTyped = UserHasTyped + "1";
     }
-    public void ButtonPressed(string buttonOperation)
+    public void ButtonPressedNumber(string buttonNumber)
     {       
-        UserHasTyped = UserHasTyped + buttonOperation;
+        UserHasTyped = UserHasTyped + buttonNumber;
+    }
+
+    public void ButtonPressedOperator(string buttonOperation)
+    {       
+        /*
+        UserPressedKey  RunningTotal    Equation
+        1                   1            1       
+        2                   12           12      
+        +                   12           12+
+        3                   3            12+3    
+        4                   34           23+34
+        -                   57           57-
+        2                   57           57-2
+        7                   57           57-27
+        =                   30
+        
+        
+        
+        */               
     }
 
 }
